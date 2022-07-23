0c0ed34f03244149b401b14fc1bc02f403c44d63
const_zpc <const_zpc@163.com>
2022-07-23 23:31:05 +0800

    🧪 test: 删除无用demo代码，添加代码注释

--- Demo/STM32/USER/main.c
+++ Demo/STM32/USER/main.c
@@ -1,3 +1,18 @@
+/***********************************************************************************************************************
+
+        程序说明：
+        
+             环境：采用 精英STM32F103 开发板 + OLED
+        OLED 引脚：DC--PC2    RES--PC1   MOSI--PC0     CLK--PC3
+             按键：KEY0--PE4  KEY1--PE3  KEY_UP--PA0
+        
+        功能：
+            1、开机则进入语言选择界面，选择后自动返回上级菜单
+            2、主菜单采用图像+描述选择
+            3、菜单选择下：KEY_UP-切换选项  KEY1-确定  KEY0-返回
+
+
+***********************************************************************************************************************/
 #include "sys.h"
 #include "oled.h"
 #include "usart.h"
@@ -11,46 +26,24 @@
 #include "mainhmi.h"
 
 
-void TestTask(void);
-
 void KEY_ScanTask(void)
 {
     KEY_Scan(10);
-}
-
-void TestTask(void)
-{
     Menu_Task();
-}
-
-void OnKeyFunCB(IoKeyType_e key, KeyAction_e action)
-{
-    static uint16_t s_cnt = 0;
-
-    if (action == KEY_ACTION_LOSSEN)
-    {
-        OLED_Printf("s_cnt:%d\r\n", s_cnt++);
-    }
 }
 
 int main(void)
 {
     SystemTickInit();
-    FML_USART_Init();
 
     OLED_Init();
     OLED_Clear(OLED_BLACK);
-    //OLED_SetText(0, 10, "test", 0, FONT_24X24);
-    OLED_ShowTask();
     cTASK_Init();
     FML_TIME_Init();
     KEY_Init();
-    KEY_Register(IO_KEY_0, OnKeyFunCB);
 
-    cTASK_Creat(TestTask, 10, 7);
     cTASK_Creat(KEY_ScanTask, 10, 6);
     cTASK_Creat(OLED_ShowTask, 100, 10);
-//    //cTASK_Creat(Menu_Task, 10, 10);
 
     MainMenuCfg_t tMainMenu;
     
